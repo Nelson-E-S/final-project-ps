7e573f0989d5dcd60f3b925787c613f6498f779a
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-09-24 10:08:36 -0400

    more switch edits

--- frontend/src/components/MySwitch.js
+++ frontend/src/components/MySwitch.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import AnimalCasePage from '../pages/AnimalCasePage';
 import SettingsForm from '../components/SettingsForm';
